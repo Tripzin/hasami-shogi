eb2a7b9d899bdd154355a2ecf5ef6c5ac26f6b69
Tripzin <kingyoikusei@gmail.com>
2020-02-12 14:59:21 +0900

    Updates

---
+++ precache-manifest.4c3bb86cda57b99ef4072d17eeb6c5db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ed378e218ea8c20066d8b1944bd1a0",
+    "url": "/hasami-shogi/index.html"
+  },
+  {
+    "revision": "9873d31e1e2b57807c56",
+    "url": "/hasami-shogi/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/hasami-shogi/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hasami-shogi/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9873d31e1e2b57807c56",
+    "url": "/hasami-shogi/static/js/main.8c052cfa.chunk.js"
+  },
+  {
+    "revision": "4c10e8a2367ad4b9ae15",
+    "url": "/hasami-shogi/static/js/runtime-main.cdf36a9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hasami-shogi/static/media/logo.5d5d9eef.svg"
+  }
+]);
